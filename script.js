7f5af634324f536404dbc224c29438507a9bdfa6
aleyna-cintron <aleynatcintron@gmail.com>
2025-04-18 22:07:04 -0400

    combat logic, and hp restrictions

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// Persistent State
 let hp = 20;
-let hpTxt = document.getElementById("hpVal");
+const hpTxt = document.getElementById("hpVal");
 hpTxt.innerHTML = hp;
-let rune;
-let prevRune = [];
-let canRunNextRune = true;
+
+let room;
+let prevRoom = [];
+let canRunNextRoom = true;
+
 const runBtn = document.getElementById("runBtn");
 runBtn.addEventListener("click", runaway);
-const nxtRuneBtn = document.getElementById("nxtRuneBtn");
-nxtRuneBtn.addEventListener("click", () => newRune(deck));
-let sword;
-let hasWeapon = false
+
+const nxtRoomBtn = document.getElementById("nxtRoomBtn");
+nxtRoomBtn.addEventListener("click", () => newRoom(deck));
+
+let hasWeapon = false;
 let cardContainer = document.getElementById("cardContainer");
-let myWep = []
+let myWep = [];
 let slainMonster;
+let slainMonsters = [];
+let usedPot;
 
 let suits = {
     diamonds: "red",
@@ -62,141 +68,180 @@ function shuffle(deck){
 }
 
 shuffle(deck);
+let cardsLeft = deck.length;
+const cardsLeftTxt = document.getElementById("cardsLeft");
+cardsLeftTxt.innerHTML = cardsLeft;
 
-function displayRune(rune) {
-    console.log("rune length", rune.length);
-    
+function displayRoom(room) {
     const dynamicCards = document.getElementById("dynamicCards");
     
     // Clear any previous cards
     dynamicCards.innerHTML = "";
 
-    for (let i = 0; i < rune.length; i++) {
+    for (let i = 0; i < room.length; i++) {
         const cardImg = document.createElement("img");
         cardImg.id = `card${i}`;
-        cardImg.src = `./PNG-cards-1.3/${rune[i].rank}_of_${rune[i].suit}.png`;
-        cardImg.alt = `${rune[i].rank} of ${rune[i].suit}`;
+        cardImg.src = `./PNG-cards-1.3/${room[i].rank}_of_${room[i].suit}.png`;
+        cardImg.alt = `${room[i].rank} of ${room[i].suit}`;
         cardImg.style.height = "200px";
         cardImg.classList.add("card"); // optional: add a class for styling
         dynamicCards.appendChild(cardImg);
     };
-    if (rune.length < 2){
-        nxtRuneBtn.disabled = false;
-    }else nxtRuneBtn.disabled = true;
-    return rune;
+    if (room.length < 2){
+        nxtRoomBtn.disabled = false;
+    }else nxtRoomBtn.disabled = true;
+    return room;
 }
-// new rune
+// new room
 
-function newRune(deck){
-    console.log("newRune func running");
-    console.log(deck.length);
-    // player can run next rune
-    if (canRunNextRune){
+function newRoom(deck){
+    // player can run next room
+    if (canRunNextRoom){
         // re-enable the button
         runBtn.disabled = false
     }
     // if player chose to run
     if (runBtn.disabled){
-        // update variable so player can run next rune
-        canRunNextRune = true;
+        // update variable so player can run next room
+        canRunNextRoom = true;
     } 
+    // reset hp
+    usedPot = null;
     // grab first 4 cards
     let removedCards = deck.splice(0,4);
-    rune = removedCards;
+    room = removedCards;
     // place these cards into an array
-    prevRune.push(...removedCards);
+    prevRoom.push(...removedCards);
     // return the first 4 cards
-    displayRune(rune);
+    displayRoom(room);
 };
 
-newRune(deck);
+newRoom(deck);
 
 // run away mechanic
-// can run away as long as you didn't run from previous rune
+// can run away as long as you didn't run from previous room
 function runaway(){
     // if button is not disabled 
     if(!runBtn.disabled){
-        console.log('start of run away function')
-        // player chose to run away meaning they can not run away on next rune
+        // player chose to run away meaning they can not run away on next room
         // disable button
         runBtn.disabled = true;
-        canRunNextRune = false;
+        canRunNextRoom = false;
         // put the first 4 cards back into deck
-        deck.push(...prevRune);
-        // get a new rune
-        console.log(prevRune);
-        console.log("did it go back in deck? ", deck);
-        // empty previous rune
-        prevRune = []
-        newRune(deck);
-        console.log('the end of the run away function');
-        
+        deck.push(...prevRoom);
+        // get a new room
+        // empty previous room
+        prevRoom = []
+        newRoom(deck);        
     }
     return runBtn;
 }
+function handleMonsterCard(card){
+    // if no wep
+    // we will subtract from main hp
+    const monsterVal = card.rank
+    if (!hasWeapon){
+        dmgTaken(monsterVal)
+        room.splice(card.index, 1);
+        displayRoom(room);
+        return;
+    } 
+    // if player has not slain a monster
+    if (!slainMonster){
+        handleWeaponAttack(card)
+        room.splice(card.index, 1);
+        displayRoom(room);
+        return;
+    }
+    // if player has slain w current weapon
+    if (slainMonster) {
+        // if monster has been slain with that sword
+        // then check if current monster is smaller than previously slain monster
+        if (card.rank < slainMonster.rank){
+            alert("we can kill");
+            // run function to calc if hp needs to take damage,
+            // also wanna update slainMonster
+            handleWeaponAttack(card);
+            room.splice(card.index, 1);
+            displayRoom(room);
+        } else {
+            alert("Monster ignores weapon and attacks you head on", slainMonster)
+            console.log("Monster ignores weapon and attacks you head on", slainMonster, "rip my card is : ", card.rank)
 
+            // have monster fully attack hp instead
+            dmgTaken(monsterVal)
+            room.splice(card.index, 1);
+            displayRoom(room);
+        } 
+    }
+}
+function handleWeaponAttack(card) {
+        // Calculate the weapon's remaining durability after the attack
+        const unblockedDamage = myWep.rank - card.rank;
+    
+        // If weapon cannot block all damage
+        if (unblockedDamage < 0) {
+            // Subtract the remaining damage from the player's HP
+            dmgTaken(unblockedDamage);
+        }
+        // monster should always be placed in slain even if not fully blocked
+        slainMonster = { suit: card.suit, rank: card.rank }; // Record the slain monster
+        slainMonsters.push(slainMonster);
+        displaySlainMonsters(slainMonsters); // Update slain monster display 
+}
+function handleHeartsCard(card){
+    if (!usedPot){
+        if (hp + card.rank > 20){
+            hp = 20
+            hpTxt.innerHTML = hp;
+        } else {hp += card.rank;
+            hpTxt.innerHTML = hp;
+        }
+        usedPot = 1;
+        room.splice(card.index, 1);
+        displayRoom(room);
+    } else {
+        alert("already used POT this room");
+    }
+}
+function handleDiamondsCard(card){
+    slainMonsters=[];
+    slainMonster = false
+    let sword = card.rank;
+    hasWeapon = true;
+    myWep = card
+    displayWeapon(myWep)
+    room.splice(card.index, 1);
+    displayRoom(room);
+    displaySlainMonsters(slainMonsters); // Update slain monster display 
+}
+let cardIndex;
 cardContainer.addEventListener("click", (event) => {
     if (event.target.id && event.target.id !== cardContainer.id) {
         const id = event.target.id; // e.g. "card3"
         const cardIndex = parseInt(id.replace("card", "")); // 3
-        for (i=0; i<rune.length; i++){
-            if (cardIndex == i){
-                const card = rune[i]
-                if (card.suit === "clubs" || card.suit === "spades") {
-                    // if no wep
-                    // we will subtract from main hp
-                    if (!hasWeapon){
-                        hp -= card.rank;
-                        hpTxt.innerHTML = hp;
-                    } else {
-                        // if you have a wep
-                        // and if you've damaged your weapon (fought with it previously then)
-                        // find out which value is bigger
-                        // do subtraction
-                        // take that value and take away from hp
-                        if (!slainMonster){
-                            myWep.rank -= card.rank;
-                            if (myWep.rank < 0){
-                                hp += myWep.rank
-                                hpTxt.innerHTML = hp;
-                                hasWeapon = false
-                                
-                            }
-                            displayWeapon(myWep);
-                            slainMonster = card.rank;
-                        } else {
-                            hp -= card.rank;
-                            hpTxt.innerHTML = hp;
-                        } 
-                    }
-                    // console.log("my hp", hp)
-                    // console.log("my rune ", rune)
-                };
-                if (card.suit === "hearts"){
-                    if (hp + card.rank > 20){
-                        hp = 20
-                        hpTxt.innerHTML = hp;
-                        console.log("my hp is capped at", hp)
-                    } else {hp += card.rank;
-                        hpTxt.innerHTML = hp;
-                    }
-                };
-                if (card.suit === "diamonds"){
-                    sword = card.rank;
-                    hasWeapon = true;
-                    console.log("card",card)
-                    myWep = card
-                    displayWeapon(myWep)
-                };
-                    rune.splice(cardIndex, 1);
-                    displayRune(rune);
-            }
+        const card = room[cardIndex];
+        card["index"] = cardIndex;
+        switch (card.suit){
+            case "clubs":
+            case "spades":
+                handleMonsterCard(card);
+                break;
+            case "hearts":
+                handleHeartsCard(card);
+                break;
+            case "diamonds":
+                handleDiamondsCard(card);
+                break;
         }
+        cardsLeft = deck.length;
+        cardsLeftTxt.innerHTML = cardsLeft;
     }
 })
 function displayWeapon(myWep){
-    const weaponContainer = document.getElementById("weaponContainer");
-    weaponContainer.innerHTML = "";
+    const weaponSlot = document.getElementById("weaponSlot");
+    console.log("this is my wep",myWep)
+    weaponSlot.innerHTML = "";
     const weaponImg = document.createElement("img");
     const weaponDmg = document.createElement("img");
     if (hasWeapon){
@@ -207,15 +252,52 @@ function displayWeapon(myWep){
         console.log("my weapon is ", myWep.rank)
         weaponDmg.style.height = "200px";
         weaponImg.style.height = "200px";
-        weaponContainer.appendChild(weaponImg);
-        weaponContainer.appendChild(weaponDmg);
+        weaponSlot.appendChild(weaponImg);
+        weaponSlot.appendChild(weaponDmg);
         
     } else {
         weaponImg.id = "weapon";
         weaponImg.src = "bareFist.gif";
         weaponImg.alt = "bare fist";
         weaponImg.style.height = "200px";
-        weaponContainer.appendChild(weaponImg);   
+        weaponSlot.appendChild(weaponImg);   
     }  
 };
 displayWeapon()
+// display slain monster 
+function displaySlainMonsters(slainMonsters) {
+    // Get the container element
+    const slainMonsterContainer = document.getElementById("slainMonsterContainer");
+
+    // Clear the container to avoid duplicates
+    slainMonsterContainer.innerHTML = "";
+
+    // Iterate through the `slainMonsters` array and create image elements
+    slainMonsters.forEach(slainMonster => {
+        const slainMonsterImg = document.createElement("img");
+        slainMonsterImg.alt = "Previously slain monster card";
+        slainMonsterImg.src = `./PNG-cards-1.3/${slainMonster.rank}_of_${slainMonster.suit}.png`;
+        slainMonsterImg.style.height = "200px";
+        slainMonsterImg.style.margin = "5px"; // Add some spacing between cards
+        slainMonsterContainer.appendChild(slainMonsterImg);
+    });
+
+    // Log for debugging
+    console.log("All slain monsters displayed:", slainMonsters);
+}   
+
+function dmgTaken(monsterVal){
+    if (monsterVal < 0){
+        hp += monsterVal; // monsterVal is negative
+        hpTxt.innerHTML = hp;
+    } else {
+        hp -= monsterVal; // monsterVal is negative
+        hpTxt.innerHTML = hp;
+    };
+    if(hp<=0){
+        alert("game over");
+        // flashing red screen
+        // run function to reset deck and refresh game
+        shuffle(deck);
+    }
+}
